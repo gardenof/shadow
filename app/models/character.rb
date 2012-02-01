6f5b9306054b9c4d7b5d9b1d1980f7dba1755e52
gardenof <edendelgado@gmail.com>
2012-02-01 12:45:35 -0500

    legality color shows on main page only for Equipped items

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,10 +3,6 @@ class Character < ActiveRecord::Base
 		has_many :game_expenses
 		validates :name, :presence => true
 
-def self.character_legality_
-	
-end
-
 	def sum_a 
 		total = 0
 		self.game_assets.each do |a|
