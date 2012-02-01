ac708dd88dab05ddea4dd143b82cf53e4726713b
gardenof <edendelgado@gmail.com>
2012-02-01 14:39:12 -0500

    cleaned up the code for legality color showing on  main page, only for Equipped items

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,9 +3,25 @@ class Character < ActiveRecord::Base
 		has_many :game_expenses
 		validates :name, :presence => true
 
+		def character_legality_status
+			send_back= ""
+			game_assets.each  do |c|
+				if c.equipped == true
+					if    c.legality == "Forbidden"   
+            @send_back_legality = c.legality 
+	        elsif c.legality == "Restricted" && @color != "Forbidden"
+            @send_back_legality = c.legality 
+	        elsif  @color != "Forbidden" && @color != "Restricted"  
+            @send_back_legality = c.legality 
+	        end 
+				end
+			end
+			@send_back_legality
+		end
+
 	def sum_a 
 		total = 0
-		self.game_assets.each do |a|
+		game_assets.each do |a|
 			if a.price.nil?
 
 			else
@@ -16,7 +32,7 @@ class Character < ActiveRecord::Base
 	end		
 	def sum_e
 		total = 0
-		self.game_expenses.each do |a|
+		game_expenses.each do |a|
 			  total = a.price + total
 		end
 	total
